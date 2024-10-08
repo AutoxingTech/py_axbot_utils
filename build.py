179a0df2a9ccc7f8cbc83f39d82b75b76228bafd
FengZhaolin <kingsimba0511@gmail.com>
2024-10-08 20:13:03 +0800

    add clean command

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import argparse
 import subprocess
+import shutil
+import os
 
 FOLDER = "py_axbot_utils"
 
@@ -8,11 +10,16 @@ parser = argparse.ArgumentParser()
 
 sub_parsers = parser.add_subparsers(dest="cmd", required=True)
 _build_parser = sub_parsers.add_parser("build")
+_build_parser = sub_parsers.add_parser("clean")
 _publisher_parser = sub_parsers.add_parser("publish")
 _translate_parser = sub_parsers.add_parser("i18n")
 
 args = parser.parse_args()
-if args.cmd == "build":
+if args.cmd == "clean":
+    for folder in ["build", "dist", f"{FOLDER}.egg-info"]:
+        if os.path.isdir(folder):
+            shutil.rmtree(folder)
+elif args.cmd == "build":
     subprocess.run(["python3", "setup.py", "sdist", "bdist_wheel"], check=True)
 elif args.cmd == "publish":
     subprocess.run("twine upload dist/*", shell=True, check=True)
