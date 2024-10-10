fea2c2faf99388d0f6ae562c2c834f6b15036209
FengZhaolin <kingsimba0511@gmail.com>
2024-10-10 20:48:43 +0800

    finish data_model

--- py_axbot_utils/validators.py
+++ py_axbot_utils/validators.py
@@ -1,5 +1,20 @@
+import numbers
+from email.utils import parseaddr
+
 from .exceptions import ValidationError
 from .translations import _
+
+
+class NumberValidator:
+    def __call__(self, value):
+        if not isinstance(value, numbers.Number):
+            raise TypeError(_("'{}' is not a number.").format(value))
+
+
+class StringValidator:
+    def __call__(self, value):
+        if not isinstance(value, str):
+            raise TypeError(_("{} is not a string.").format(value))
 
 
 class RangeValidator:
@@ -10,10 +25,21 @@ class RangeValidator:
     def __call__(self, value):
         if self.min_value and value < self.min_value:
             raise ValidationError(
-                _("Value {} should not be smaller than{}").format(self.min_value)
+                _("Value '{}' should not be smaller than {}").format(
+                    value, self.min_value
+                )
             )
 
         if self.max_value and value > self.max_value:
             raise ValidationError(
-                _("Value {} should not be larger than{}").format(self.max_value)
+                _("Value '{}' should not be larger than {}").format(
+                    value, self.max_value
+                )
             )
+
+
+class EmailValidator:
+    def __call__(self, value):
+        _name, addr = parseaddr(value)
+        if "@" not in addr:
+            raise ValidationError(_("'{}' is not a valid email address").format(value))
