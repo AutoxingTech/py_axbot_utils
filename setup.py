44247155b41491331f3eaad3bceb141748070ee2
FengZhaolin <kingsimba0511@gmail.com>
2025-03-11 20:40:21 +0800

    change version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="py_axbot_utils",  # Replace with your package name
-    version="0.1.2",
+    version="0.1.3",
     author="AutoXing",
     author_email="someone@autoxing.com",
     description="Utilities for AutoXing Robotics",
